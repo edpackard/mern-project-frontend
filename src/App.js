08a37f302b1615c4c89e237a767ef5e52a170036
edpackard <edpackard81@gmail.com>
2021-11-17 23:02:53 +0000

    Note component moved to own module

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import React from "react";
-
-const Note = ({ note }) => {
-  return <li>{note.content}</li>;
-};
+import Note from "./components/Note";
 
 const App = (props) => {
   const { notes } = props;
