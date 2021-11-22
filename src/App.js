ad70295215b64777cef289c9f06fe9ce3769c221
edpackard <edpackard81@gmail.com>
2021-11-22 13:22:45 +0000

    Extract backend communication into separate module

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import Note from "./components/Note";
-import axios from "axios";
+import noteService from "./services/notes";
+// import axios from "axios";
 
 const App = () => {
   const [notes, setNotes] = useState([]);
@@ -8,13 +9,19 @@ const App = () => {
   const [showAll, setShowAll] = useState(true);
 
   useEffect(() => {
-    console.log("effect");
-    axios.get("http://localhost:3001/notes").then((response) => {
-      console.log("promise fulfilled");
-      setNotes(response.data);
+    noteService.getAll().then((initialNotes) => {
+      setNotes(initialNotes);
     });
   }, []);
   console.log("render", notes.length, "notes");
+
+  const toggleImportanceOf = (id) => {
+    const note = notes.find((n) => n.id === id);
+    const changedNote = { ...note, important: !note.important };
+    noteService.update(id, changedNote).then((returnedNote) => {
+      setNotes(notes.map((note) => (note.id !== id ? note : returnedNote)));
+    });
+  };
 
   const addNote = (event) => {
     event.preventDefault();
@@ -24,22 +31,13 @@ const App = () => {
       important: Math.random() < 0.5,
     };
 
-    axios.post("http://localhost:3001/notes", noteObject).then((response) => {
-      setNotes(notes.concat(response.data));
+    noteService.create(noteObject).then((returnedNote) => {
+      setNotes(notes.concat(returnedNote));
       setNewNote("");
     });
   };
 
   const notesToShow = showAll ? notes : notes.filter((note) => note.important);
-
-  const toggleImportanceOf = (id) => {
-    const url = `http://localhost:3001/notes/${id}`;
-    const note = notes.find((n) => n.id === id);
-    const changedNote = { ...note, important: !note.important };
-    axios.put(url, changedNote).then((response) => {
-      setNotes(notes.map((note) => (note.id !== id ? note : response.data)));
-    });
-  };
 
   const handleNoteChange = (event) => {
     setNewNote(event.target.value);
