13cb1237d9afa385cb52ebba3ca58b683751e14d
edpackard <edpackard81@gmail.com>
2021-11-23 17:50:50 +0000

    Update baseUrl for deployment

--- src/services/notes.js
+++ src/services/notes.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const baseUrl = "https://damp-tor-55947.herokuapp.com/api/notes";
+const baseUrl = "/api/notes";
 
 const getAll = () => {
   const request = axios.get(baseUrl);
